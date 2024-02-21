2ed4fd2c1d09baba7f02a56932736669b0859d6b
huangjialong <2025532319@qq.com>
2024-02-21 22:14:14 +0800

    20240221-start

---
+++ farmers_help-application/src/main/java/farmer/help/FarmerHelpApplication.java
@@ -0,0 +1,17 @@
+package farmer.help;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @description: 启动类
+ * @author：dukelewis
+ * @date: 2024/2/21
+ * @Copyright： https://github.com/DukeLewis
+ */
+@SpringBootApplication
+public class FarmerHelpApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FarmerHelpApplication.class, args);
+    }
+}
